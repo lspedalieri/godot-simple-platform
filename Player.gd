be7c91e42b4547645728d4bc7a77ac77ea0589bf
Leopoldo Spedalieri <lspedalieri@contents.com>
2022-07-26 01:36:16 +0200

    Part4 - Animation Sprite, Sprite Frames, fix jump animation when not on floor, flip sprite on left right changing

--- Player.gd
+++ Player.gd
@@ -6,15 +6,27 @@ const JUMPFORCE = -900
 const GRAVITY = 30
 
 func _physics_process(delta):
+
 	if Input.is_action_pressed("left_keys"):
 		velocity.x = -SPEED
-	if Input.is_action_pressed("right_keys"):
+		$Sprite.flip_h = true
+		$Sprite.play("walk")
+	elif Input.is_action_pressed("right_keys"):
 		velocity.x =  SPEED
-		
+		$Sprite.flip_h = false
+		$Sprite.play("walk")
+	else:
+		$Sprite.play("idle")
+
+	if not is_on_floor():
+		$Sprite.play("air")
+			
 	velocity.y += GRAVITY
 	
 	if Input.is_action_just_pressed("jump_keys") and is_on_floor():
 		velocity.y = JUMPFORCE
+		
+
 	#returning velocity frame by frame, we stop acceleration after a vertical collision
 	#the second parameter fix the jump, setting where is the ceiling
 	velocity = move_and_slide(velocity, Vector2.UP)
