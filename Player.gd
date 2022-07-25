ea3158afff255621586561f6b5bd31be8d894855
Leopoldo Spedalieri <lspedalieri@contents.com>
2022-07-26 01:14:41 +0200

    Part 3 - Jump & gravity, fix mid-air jump and constant falling acceleration. Setting speed constants

--- Player.gd
+++ Player.gd
@@ -1,12 +1,22 @@
 extends KinematicBody2D
 
 var velocity = Vector2(0,0)
+const SPEED = 180
+const JUMPFORCE = -900
+const GRAVITY = 30
 
 func _physics_process(delta):
 	if Input.is_action_pressed("left_keys"):
-		velocity.x = -100
+		velocity.x = -SPEED
 	if Input.is_action_pressed("right_keys"):
-		velocity.x =  100
-	move_and_slide(velocity)
+		velocity.x =  SPEED
+		
+	velocity.y += GRAVITY
+	
+	if Input.is_action_just_pressed("jump_keys") and is_on_floor():
+		velocity.y = JUMPFORCE
+	#returning velocity frame by frame, we stop acceleration after a vertical collision
+	#the second parameter fix the jump, setting where is the ceiling
+	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	velocity.x = lerp(velocity.x, 0, 0.2)
