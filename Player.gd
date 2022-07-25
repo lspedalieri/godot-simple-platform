4ef0d6ab954a66e8fa22817f18106dee8a678942
Leopoldo Spedalieri <lspedalieri@contents.com>
2022-07-26 00:39:14 +0200

    Capitolo 2 - linked scene, linked script, conditional statement, input map, move and slide, lerp

---
+++ Player.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+
+var velocity = Vector2(0,0)
+
+func _physics_process(delta):
+	if Input.is_action_pressed("left_keys"):
+		velocity.x = -100
+	if Input.is_action_pressed("right_keys"):
+		velocity.x =  100
+	move_and_slide(velocity)
+	
+	velocity.x = lerp(velocity.x, 0, 0.2)
