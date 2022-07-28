301b37bb11e25edfb4cfbc1673b7ea863268299c
Leopoldo Spedalieri <lspedalieri@contents.com>
2022-07-29 01:53:20 +0200

    Part12

--- Coin.gd
+++ Coin.gd
@@ -1,11 +1,15 @@
 extends Area2D
 
-
+signal coin_collected
 
 func _on_Coin_body_entered(body):
 	$AnimationPlayer.play("bounce")
-	body.add_coin()
+	emit_signal("coin_collected")
+	set_collision_mask_bit(0, false)
+	
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	queue_free()
+
+
