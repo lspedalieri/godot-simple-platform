8dd7bd48f8c53f1547154f9ffd5445ad51d29a1d
Leopoldo Spedalieri <lspedalieri@contents.com>
2022-07-31 01:25:36 +0200

    Part 13 - Creating Enemies & RayCast2D

--- Coin.gd
+++ Coin.gd
@@ -3,9 +3,9 @@ extends Area2D
 signal coin_collected
 
 func _on_Coin_body_entered(body):
+	set_collision_mask_bit(0, false)
 	$AnimationPlayer.play("bounce")
 	emit_signal("coin_collected")
-	set_collision_mask_bit(0, false)
 	
 
 
